51ade48b0e7aecccee593652403eb7f046b00cf9
Serhii Tatarintsev <sergey@tatarintsev.me>
2024-05-11 16:20:09 +0200

    VIMode in the prompt

--- home.nix
+++ home.nix
@@ -24,7 +24,9 @@
 
         zsh = {
             enable = true;
+            defaultKeymap = "viins";
             initExtra = ''
+                KEYTIMEOUT=5
                 autoload -U colors
                 zstyle ':completion:*' menu select
             '';
@@ -40,6 +42,12 @@
             settings = {
                 format = "$all";
                 right_format = "$git_branch$git_commit$git_state$git_status";
+
+                character = {
+                    success_symbol = "[ I ](black bg:green) [❯](green)";
+                    error_symbol = "[ I ](black bg:green) [❯](red)";
+                    vimcmd_symbol = "[ N ](black bg:blue) [❯](green)";
+                };
                 git_branch = {
                     format = "[$symbol$branch(:$remote_branch)]($style) ";
                 };
